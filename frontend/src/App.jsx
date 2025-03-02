7a66d60d730f1af6a880b9a2739d49898c3bb30c
Rushikesh <pardhikar26@gmail.com>
2025-03-02 14:11:57 +0530

    project folder str created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    
+      <Button>Let create LMS</Button>
+    
+  )
+}
+
+export default App
