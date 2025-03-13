239c9a5f2b0b0bb44187c35abeb8dfb04c7d10ba
Rushikesh <pardhikar26@gmail.com>
2025-03-13 20:53:29 +0530

    update course

--- frontend/src/pages/student/Courses.jsx
+++ frontend/src/pages/student/Courses.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Skeleton } from "@/components/ui/skeleton";
 import { Course } from './Course';
 
+const courses = [1,2,3,4,5,6]
+
 export const Courses = () => {
   const isLoading = false;
   return (
@@ -10,9 +12,11 @@ export const Courses = () => {
           <h1 className='font-bold text-3xl text-center mb-10'>Our Courses</h1>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
             {
-              isLoading ? Array.from({length:8}).map((_,index) => (
+              isLoading ? ( Array.from({length:8}).map((_,index) => (
                 <CourseSkeleton key={index}/>  
-              )) : <Course/>
+              ))) : (
+                courses.map((course,index)=> )
+               )
             }
           </div>
         </div>
