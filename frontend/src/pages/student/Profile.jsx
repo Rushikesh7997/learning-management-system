a7c231c68ee2f37deb2d06919b07d813e805d569
Rushikesh <pardhikar26@gmail.com>
2025-03-14 22:53:41 +0530

    work on profile page

--- frontend/src/pages/student/Profile.jsx
+++ frontend/src/pages/student/Profile.jsx
@@ -1,3 +1,4 @@
+import { AvatarImage, Avatar, AvatarFallback } from '@/components/ui/avatar'
 import React from 'react'
 
 export const Profile = () => {
@@ -5,7 +6,14 @@ export const Profile = () => {
     <div className='max-w-xl mx-auto my-24 px-4 md:px-0 '>
         <h1 className='font-bold text-2xl text-center md:text-left'>My 
           Profile</h1>
-
+          <div className='flex flex-col md:flex-row items-center md:items-center gap-8 my-5'>
+            <div className=''>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" alt="" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          </div>
     </div>
   )
 }
