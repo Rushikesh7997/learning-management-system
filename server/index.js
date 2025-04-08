3388bd156548d60e7e0a750da4f0cf2e67cb2fc8
Rushikesh <pardhikar26@gmail.com>
2025-04-09 01:20:57 +0530

    work on course model schema controller route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js"
+import courseRoute from "./routes/course.route.js"
 
 dotenv.config({})
 
@@ -25,6 +26,7 @@ app.use(cors({
 
 // Apis
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute)
 
 app.listen(PORT, ()=>{
     console.log(`Server listen at port ${PORT}`);
