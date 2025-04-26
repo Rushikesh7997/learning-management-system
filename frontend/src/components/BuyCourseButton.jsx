34fd0f2e081cdd7525374e49f64e0607d999b703
Rushikesh <pardhikar26@gmail.com>
2025-04-26 22:12:21 +0530

    work on payment mode

--- frontend/src/components/BuyCourseButton.jsx
+++ frontend/src/components/BuyCourseButton.jsx
@@ -1,6 +1,12 @@
+import { useCreateCheckoutSessionMutation } from '@/features/api/purchaseApi'
 import React from 'react'
 
 export const BuyCourseButton = () => {
+  const [createCheckoutSession, {isLoading}] = useCreateCheckoutSessionMutation();
+
+  const  purchaseCourseHandler = async () =>{
+    await createCheckoutSession();
+  }
   return (
         <Button className="w-full">Buy Now</Button>
   )
